e0be342817ce18eb7d11b9ace77e6c4b5824f31a
Hugi Thordarson <hugi@karlmenn.is>
2026-02-25 20:52:03 +0000

    Delete unused logic

--- wolips/plugins/ng.componenteditor/java/org/objectstyle/wolips/eomodeler/core/model/ISortableEOModelObject.java
+++
@@ -1,7 +0,0 @@
-package org.objectstyle.wolips.eomodeler.core.model;
-
-public interface ISortableEOModelObject {
-	public String getName();
-	
-	public void setName(String name) throws DuplicateNameException;
-}
